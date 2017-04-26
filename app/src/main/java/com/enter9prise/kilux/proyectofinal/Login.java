d8eca680dcb90af7b60804e36ea4ad5e515e49d0
Jorge <jorgeloquendo2013@outlook.com>
2017-04-25 22:19:02 -0500

    hola

---
+++ app/src/main/java/com/enter9prise/kilux/proyectofinal/Login.java
@@ -0,0 +1,13 @@
+package com.enter9prise.kilux.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ventana_login);
+    }
+}
